fa5a785d2786fd95f5c90e2a0a8606a7b16b2162
IMvErickz <erickspy2003@gmail.com>
2022-12-30 14:33:21 -0300

    teste3: axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://us-east.connect.psdb.cloud/formdev?sslaccept=strict'
+    baseURL: 'https://us-east.connect.psdb.cloud/formdev?sslaccept=strict'
 })
